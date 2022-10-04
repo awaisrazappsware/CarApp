cb7e09b1dc7e40be1a799231587b96bf86316de2
awaisappsware <awaisrazaappsware@gmail.com>
2022-10-04 18:03:33 +0500

    inituials

--- app/src/main/java/com/awais/raza/car/app/ui/edit/EditFragment.kt
+++ app/src/main/java/com/awais/raza/car/app/ui/edit/EditFragment.kt
@@ -24,7 +24,6 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import java.util.*
 
-
 class EditFragment : Fragment() {
 
     private lateinit var binding: FragmentEditBinding
